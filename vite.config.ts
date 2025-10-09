de9d4f760ca5b6244fe0cfc8dd4a08c65dd88e37
BombayV <OMGmsebastian@gmail.com>
2025-10-09 14:08:47 -0400

    feat: dev project

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: 'build.html',
-      output: {
-        assetFileNames: 'assets/[name].[ext]',
-        chunkFileNames: 'assets/[name].js',
-        entryFileNames: 'assets/[name].js'
-      }
-    },
-    outDir: 'dist'
-  },
   server: {
     fs: {
       allow: ['..'],
